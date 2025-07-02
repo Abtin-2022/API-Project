64edb4fd1be4dedfc3c64638243bc5f5606f7494
luciano093 <luparedes100@gmail.com>
2025-07-02 19:40:22 -0400

    Remove unnecessary sqlalchemy from tests.py

--- tests.py
+++ tests.py
@@ -1,5 +1,4 @@
 import unittest
-import sqlalchemy as db
 
 from file1 import article_access, content_extractor, insert_to_db, link_extractor
 
@@ -20,16 +19,6 @@ class TestArticleFunctions(unittest.TestCase):
     def test_content_extractor(self):
         content = [{'content': 'This is a test article.'}]
         self.assertEqual(content_extractor(content), 'This is a test article.')
-
-    def test_insert_to_db(self):
-        engine = db.create_engine('sqlite:///out.db')
-
-        insert_to_db("general", "This is a test article.", engine)
-
-        with engine.connect() as connection:
-            result = connection.execute("SELECT * FROM articles WHERE category='general'").fetchall()
-            self.assertEqual(len(result), 1)
-            self.assertEqual(result[0]['content'], "This is a test article.")
 
 if __name__ == '__main__':
     unittest.main()
